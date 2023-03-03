951f09b20d2dc0f6e1a5eb0fee437386d0a86ff1
AzizHF883 <azizhf9596@gmail.com>
2023-03-03 17:57:50 +0600

    Modified Commit for calculator

--- flaskApplication.py
+++ flaskApplication.py
@@ -3,27 +3,23 @@ from flask import Flask , request
 
 app =Flask(__name__)
 
-@app.route('/user/<name>')
-def user(name):
-	return '<h1>Hello, %s!</h1>' % name
-
-@app.route('/cal/<num1>/<op>/<num2>')
+@app.route('/cal/<int:num1>/<op>/<int:num2>')
 def cal(num1, op, num2):
-    n1= int(num1)
-    n2= int(num2)
+    #n1= int(num1)
+    #n2= int(num2)
     if op=="+":
-        result= n1+n2
+        result= num1+num2
     elif op== "-":
-        result= n1-n2
+        result= num1-num2
     elif op == '/':
-        result = n1 / n2
+        result = num1 / num2
     elif op == '*':    
-        result= n1*n2
+        result= num1*num2
     elif op == "%":
-        result= n1%n2
+        result= num1%num2
     else:
          print("Invalid Operator!")
-    return f'<h1>Result: {result}!</h1>'
+    return f'<h1>Result of {num1} {op} {num2} is {result}!</h1>'
 
 if __name__== '__main__':
     app.run(debug=True)
