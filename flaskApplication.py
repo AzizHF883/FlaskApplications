49693e7dfcbe36a7f5abf46e88f93483f627df10
AzizHF883 <azizhf9596@gmail.com>
2023-03-08 23:56:25 +0600

    Calculator updated with bootstrap

--- flaskApplication.py
+++ flaskApplication.py
@@ -1,7 +1,9 @@
 #FlaskWebAppplicationDevelopment
 from flask import Flask , render_template
+from flask_bootstrap import Bootstrap
 
 app =Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/index')
 def index():
