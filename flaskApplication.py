5c6b1c1a40b5f567b652c7f87c06c30c786129be
AzizHF883 <azizhf9596@gmail.com>
2023-03-08 20:06:50 +0600

    Flask Calculator Update with render template

--- flaskApplication.py
+++ flaskApplication.py
@@ -1,12 +1,19 @@
 #FlaskWebAppplicationDevelopment
-from flask import Flask , request
+from flask import Flask , render_template
 
 app =Flask(__name__)
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
 @app.route('/cal/<int:num1>/<op>/<int:num2>')
 def cal(num1, op, num2):
-    #n1= int(num1)
-    #n2= int(num2)
+    
     if op=="+":
         result= num1+num2
     elif op== "-":
@@ -19,7 +26,8 @@ def cal(num1, op, num2):
         result= num1%num2
     else:
          print("Invalid Operator!")
-    return f'<h1>Result of {num1} {op} {num2} is {result}!</h1>'
+    return render_template('calculator.html', num1=num1, op=op, num2=num2, result=result)
+    #return f'<h1>Result of {num1} {op} {num2} is {result}!</h1>'
 
 if __name__== '__main__':
     app.run(debug=True)
